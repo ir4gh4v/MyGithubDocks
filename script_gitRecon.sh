832fc24db512f8c413af931cb6d9bf97134f32ee
pacho15 <96167320+pacho15@users.noreply.github.com>
2023-01-09 00:03:20 +0530

    Update script_gitRecon.sh

--- script_gitRecon.sh
+++ script_gitRecon.sh
@@ -4,13 +4,13 @@ host=$1
 github_recon() {
 for domain in $(cat $host);
 do
-mkdir -p /root/github-recon/$domain
+mkdir -p github-results
 echo -e "\n${CYAN}
 \t+-+-+-+-+-+-+
 \t|R|4|G|H|4|V|
 \t+-+-+-+-+-+-+
 ${RESET}\n"
-python3 GitDorker.py -tf token.txt -q $domain -d /root/tools/wordlists/keywords.txt -o /root/github-recon/$domain/results.txt
+python3 GitDorker.py -tf token.txt -q $domain -d /root/tools/wordlists/keywords.txt -o github-results/$domain/results.txt
 done
 }
 github_recon
